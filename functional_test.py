16dd352b683bf733df8959907c07998647e42f6a
seon54 <seonhyeongpark54@gmail.com>
2019-08-10 22:48:18 +0900

    modify functional_test with unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,62 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(executable_path='./chromedriver.exe')
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path='../chromedriver.exe')
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 웹 사이트 확인
+        self.browser.get('http://localhost:8000')
+
+        # 웹 페이지 타이틀과 헤더가 'To-Do' 표시
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish teh test!')
+
+        # '공작깃털 사기' 텍스트 상자 입력
+
+        # 엔터키 누르면 페이지가 갱신되고 작업 목록에 '1: 공작깃털 사기' 아이템 추가
+
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
+        # 다시 '공작깃털을 이용해서 그물 만들기' 입력
+
+        # 페이지는 다시 갱신되고, 두 개의 아이템이 목록에 보인다
+        # 사이트에서 입력한 목록을 위한 특정 URL과 설명 함께 지공
+
+        # 해당 URL에 접속하면 작업 목록 확인 가능
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+'''
+< 1장 >
+browser = webdriver.Chrome(executable_path='../chromedriver.exe')
+
+# 웹 사이트 확인
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# 웹 페이지 타이틀과 헤더가 'To-Do' 표시
+assert 'To-Do' in browser.title, 'Brower title was ' + browser.title
+
+# '공작깃털 사기' 텍스트 상자 입력
+
+# 엔터키 누르면 페이지가 갱신되고 작업 목록에 '1: 공작깃털 사기' 아이템 추가
+
+# 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
+# 다시 '공작깃털을 이용해서 그물 만들기' 입력
+
+# 페이지는 다시 갱신되고, 두 개의 아이템이 목록에 보인다
+# 사이트에서 입력한 목록을 위한 특정 URL과 설명 함께 지공
+
+# 해당 URL에 접속하면 작업 목록 확인 가능
+
+browser.quit()
+'''
