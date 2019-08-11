1ec4d8dd4cddd4d4135d6d4bcacf449a0a86eb64
seon54 <seonhyeongpark54@gmail.com>
2019-08-11 23:12:58 +0900

    add function on functional_test

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 웹 사이트 확인
         self.browser.get('http://localhost:8000')
@@ -31,37 +36,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # 엔터키 누르면 페이지가 갱신되고 작업 목록에 '1: 공작깃털 사기' 아이템 추가
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
         time.sleep(10)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        # self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows),
-        #                 '신규 작업이 테이블에 표시되지 않는다 -- 해당 텍스트:\n%s' % (table.text,))
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
         # 다시 '공작깃털을 이용해서 그물 만들기' 입력
-        self.fail('Finish the test!')
-
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
-        # 페이지는 다시 갱신되고, 두 개의 아이템이 목록에 보인다
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn('2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows])
+        # 페이지는 갱신되고 두 개의 아이템이 목록에 보인다.
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 사이트에서 입력한 목록을 위한 특정 URL과 설명 함께 제공
-        self.fail('Finish the test!')
 
-        # 해당 URL에 접속하면 작업 목록 확인 가능
+
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
 
 '''
 < 1장 >
