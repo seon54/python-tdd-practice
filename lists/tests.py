a87452607f2d2c3001902187c771c9652b35f4de
seon54 <seonhyeongpark54@gmail.com>
2019-08-11 17:16:01 +0900

    modify homoe_page view with template

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
+
+
+def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    expected_html = render_to_string('home.html')
+    self.assertEqual(response.content.decode(), expected_html)
 
 
 class HomePageTest(TestCase):
@@ -16,4 +24,4 @@ class HomePageTest(TestCase):
         response = home_page(request)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
