5545e9a5b0848c43c25c1540e4fab507da2be315
shpark <jlk@shpark.local>
2019-12-24 14:48:20 +0900

    Add unittest on functional_test.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,41 @@
 import os
+import unittest
 from selenium import webdriver
 
-path = os.path.join(os.getcwd() + '/..', 'geckodriver')
-browser = webdriver.Firefox(executable_path=path)
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        path = os.path.join(os.path.dirname(os.getcwd()), 'geckodriver')
+        self.browser = webdriver.Firefox(executable_path=path)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 에디스는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고 웹사이트를 확인하러 간다.
+        self.browser.get('http://localhost:8000')
+
+        # 웹 페이지 타이틀과 헤더가 'To-Do' 표시
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # 작업 추가
+
+        # '공작깃털 사기'라고 텍스트 상자에 입력
+
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
+        # '1: 공작깃털 사기' 아이템 추가
+
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
+        # 다시 '공작깃털을 이용해서 그물 만들기' 입력
+
+        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보임
+        # 입력한 목록을 저장하는 URL 생성
+
+        # 해당 URL에 접속하면 작업 목록 확인 가능
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
