62408e8101d3bb19ea4fd85538e1a107b27a95fe
shpark <jlk@shpark.local>
2019-12-31 11:19:56 +0900

    Check if functional test can add list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 
@@ -19,17 +20,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # 웹 페이지 타이틀과 헤더가 'To-Do' 표시
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 작업 추가
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
         # '공작깃털 사기'라고 텍스트 상자에 입력
+        inputbox.send_keys('공작깃털 사')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # '1: 공작깃털 사기' 아이템 추가
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows), )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
         # 다시 '공작깃털을 이용해서 그물 만들기' 입력
+        self.fail('Finish teh test!')
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보임
         # 입력한 목록을 저장하는 URL 생성
