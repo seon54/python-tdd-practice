e913ae4ca298ff6a5cd927c5358ccbbf0b5b0097
shpark <jlk@shpark.local>
2019-12-31 15:09:25 +0900

    Create main html using template

--- functional_tests.py
+++ functional_tests.py
@@ -27,8 +27,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
-        # '공작깃털 사기'라고 텍스트 상자에 입력
-        inputbox.send_keys('공작깃털 사')
+        # '공작깃털 사기'라고 텍스트 상자에 입력기
+        inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # '1: 공작깃털 사기' 아이템 추가
@@ -36,11 +36,11 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows), )
+        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows), "신규 작업 작업이 테이블에 표시되지 않는다")
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
         # 다시 '공작깃털을 이용해서 그물 만들기' 입력
-        self.fail('Finish teh test!')
+        self.fail('Finish the test!')
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보임
         # 입력한 목록을 저장하는 URL 생성
