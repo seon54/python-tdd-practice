251aa21fa401f6046c13ed8ba5d14ebb3cae1cd7
seon54 <seonhyeongpark54@gmail.com>
2020-01-14 16:32:09 +0900

    Add a new test for creating a new list

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,5 +6,6 @@ from lists import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('lists/the-only-list-in-the-world/', views.view_list, name='view-list'),
+    path('lists/new', views.new_list, name='new-list'),
     path('admin/', admin.site.urls),
 ]
