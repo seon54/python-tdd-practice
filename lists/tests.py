ff7f1cdc94fdc43d016136c55373dbe9ffdaae6c
seon54 <seonhyeongpark54@gmail.com>
2020-01-14 15:10:35 +0900

    Add a new test, url and view

--- lists/tests.py
+++ lists/tests.py
@@ -42,22 +42,13 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('item 1', response.content.decode())
-        self.assertIn('item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
@@ -76,3 +67,15 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫번째 아이템')
         self.assertEqual(second_saved_item.text, '두번째 아이템')
+
+
+class LiveViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
--- superlists/urls.py
+++ superlists/urls.py
@@ -5,5 +5,6 @@ from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view-list'),
     path('admin/', admin.site.urls),
 ]
