334bf5da91a977f3180f239417016a92ae14bcfd
seon54 <seonhyeongpark54@gmail.com>
2020-01-14 18:36:14 +0900

    Add a urls.py

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from lists import views
-
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', views.view_list, name='view-list'),
-    path('lists/new', views.new_list, name='new-list'),
+    path('lists/', include('lists.urls')),
     path('admin/', admin.site.urls),
 ]
