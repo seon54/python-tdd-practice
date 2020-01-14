6aa1f40e5fb20bc9fa634e0ee31c572716df2ffd
seon54 <seonhyeongpark54@gmail.com>
2020-01-14 16:40:32 +0900

    Update form with URL

--- lists/tests.py
+++ lists/tests.py
@@ -23,11 +23,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(remove_csrf_tag(response.content.decode()), remove_csrf_tag(expected_html))
 
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
